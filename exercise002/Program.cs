62ef1a12ff37eab7b8f319d6862989110e58a118
Natalya <buzlik.n@gmail.com>
2022-08-07 23:13:39 +0400

    Задачи

--- exercise002/Program.cs
+++ exercise002/Program.cs
@@ -4,7 +4,7 @@
 // Функция, на вход получает параметр, на выходе введенное с консоли значение
 int Prompt(string message)
 {
-    System.Console.Write(message);          // Вывести сообщение на экран
+    System.Console.Write(message);          // Вывести сообщение
     string readValue = Console.ReadLine();  // Считывает с консоли строку
     int result = int.Parse(readValue);      // Преобразует строку в целое число
     return result;                          // Возвращает результат
---
+++ exercive004/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N. 5 -> 2, 4
+
+// Функция, на вход получает параметр, на выходе введенное с консоли значение
+int Prompt(string message)
+{
+    System.Console.Write(message);          // Вывести сообщение
+    string readValue = Console.ReadLine();  // Считывает с консоли строку
+    int result = int.Parse(readValue);      // Преобразует строку в целое число
+    return result;                          // Возвращает результат
+}
+
+int num1;                                   // Объявление переменной 
+num1 = Prompt("Введите число N > ");        // Вводим значение
+if (num1 > 1)
+{
+    for (int count = 2; count <= num1; count = count + 2)
+    {
+        System.Console.Write($"{count} ");   // Вывод результата
+    }
+}
+else
+{
+    System.Console.WriteLine($"Число {num1} не соотвествует условию задачи");   // Вывод результата
+}
